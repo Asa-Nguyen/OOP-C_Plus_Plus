3f92e25954dbea7aa8db6d48e8fd8df8f0205e67
hung-dev <hung28122003cv@gmail.com>
2023-01-07 21:11:03 +0700

    Third commit

--- Training exercises/Geometry/Circle.cpp
+++ Training exercises/Geometry/Circle.cpp
@@ -1,1 +1,15 @@
 #include "Geometry.cpp"
+#define PI 3.14
+class Circle{
+    private:
+        int r;
+    public:
+        Circle(){}
+        Circle(int r) : r(r){};
+        double perimeter(){
+            return 2*PI*r;
+        }
+        double area(){
+            return r*r*PI;
+        }
+};
