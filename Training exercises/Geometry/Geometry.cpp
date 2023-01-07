8075f4fc9d15c5083378e978dc6a59837ecc63a2
hung-dev <hung28122003cv@gmail.com>
2023-01-07 21:04:34 +0700

    commit second

--- Training exercises/Geometry/Geometry.cpp
+++ Training exercises/Geometry/Geometry.cpp
@@ -1,2 +1,8 @@
 #include "bits/stdc++.h"
 using namespace std;
+class Geometry{
+    public:
+        virtual double perimeter() = 0;
+        virtual double area() = 0;
+        virtual void display();
+};
